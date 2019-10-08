43c32c59136b2d95eae2639406b873b5aa67b13b
samueljames <samuel4abiodun@gmail.com>
2019-10-08 23:11:48 +0200

    Allow setting of config value throgh env

--- app/Config.js
+++ app/Config.js
@@ -1,4 +1,5 @@
+
 module.exports = {
-  DB: 'mongodb://localhost:27017/todos',
-  APP_PORT: 4000
-}
+  DB: process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://localhost:27017/todos',
+  APP_PORT: process.env.APP_PORT ? process.env.APP_PORT : 4000,
+};
